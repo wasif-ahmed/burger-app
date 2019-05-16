4df1749fd6474d4b51eb3bf336fbdf89b840df59
wasifahmed37 <wasifshaikh1994@gmail.com>
2019-05-16 12:34:23 +0530

    first commit - added burger app

---
+++ build/precache-manifest.a069ffbd80ef4eb02d3a75e9b7926968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1667dfa3ca2c6d0af0ebc99d646ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7cd3d189cf7eb250eea",
+    "url": "/static/css/main.c9dde8a8.chunk.css"
+  },
+  {
+    "revision": "993afd4ef6a30aaa5789",
+    "url": "/static/js/2.1ef865f0.chunk.js"
+  },
+  {
+    "revision": "f7cd3d189cf7eb250eea",
+    "url": "/static/js/main.3ac0c05a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
