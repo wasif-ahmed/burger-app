e2c080550e9bf1124eae413f53815a95840427a3
wasifahmed37 <wasifshaikh1994@gmail.com>
2019-05-16 15:37:28 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Layout from './components/Layout/Layout';
-import BurgerBuilder from './container/BurgerBuilder/Burgerbuilder';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
